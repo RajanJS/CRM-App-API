7d8595eb6fe078ad93ddf045fea74f4f64334d84
RajanJS <rajan@spark.com.np>
2016-03-25 07:30:09 +0545

    project structure modifed

---
+++ config.js
@@ -0,0 +1,7 @@
+'use strict';
+
+ module.exports = {
+ 'port': process.env.PORT || 8080, // set the port for our app
+ 'database': 'mongodb://crm-user:crm-user@jello.modulusmongo.net:27017/nOmez2iz',
+ 'secret': 'crm-app-api' // CREATE SECRET to create TOKEN With
+ };
